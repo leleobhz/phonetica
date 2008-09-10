5fab60674d0064f69f6f73448bb3491cdd83acb2
Leonardo Silva Amaral <contato@leonardoamaral.com.br>
2008-09-10 16:34:52 +0000

    * Now it prints ONLY F0, formants and bandwidths for frames marked has containing voice. Thanks to nosklo@freenode.#python-br :]
    git-svn-id: svn://svn.code.sf.net/p/setfon/code/trunk@32 0b32ef8a-0316-45cc-9536-08b9f7b8220c

--- high_experimental/snack_demo.py
+++ high_experimental/snack_demo.py
@@ -17,6 +17,7 @@
 
 # A Sample code for Python Snack
 
+from itertools import izip
 from Tkinter import *
 root = Tk()
 import tkSnack
@@ -29,6 +30,7 @@ mysound = tkSnack.Sound()
 #mysound.read('test_praat.wav')
 mysound.read('Fn-ST-1.wav')
 
+# Extract Formants
 # Python Snack have the function formant, but dont accept any options, so we use a tk.call direct, giving access to all powerfull tk implementation. Above is all default values, and have too -windowlength, -start, -end and -lpctype 0 (autocorrelation) or 1 (stabilized covariance)
 
 formant = dict()
@@ -38,12 +40,6 @@ formant['fields'] = ('F1', 'F2', 'F3', 'F4', 'BW1', 'BW2', 'BW3', 'BW4')
 formant['time'] = formant['windowlenght'] / 2
 
 formant['processed'] = mysound.tk.call(mysound, 'formant', '-numformants', '4', '-framelength', formant['framelenght'], '-windowtype', 'Cos^4', '-windowlength', formant['windowlenght'], '-preemphasisfactor', '0.7', '-lpcorder', '12', '-ds_freq', '10000', '-nom_f1_freq', '500')
-
-for formant_local in formant['processed']:
-
-	formants = dict(zip(formant['fields'], formant_local))
-	print '\tSecond %.2f\n\t\tF1: %.0f Hz\n\t\tF2: %.0f Hz\n\t\tF3: %.0f Hz\n\t\tF4: %.0f Hz\n\t\tBW1: %.0f Hz\n\t\tBW2: %.0f Hz\n\t\tBW3: %.0f Hz\n\t\tBW4: %.0f Hz' % (formant['time'], formants['F1'], formants['F2'], formants['F3'], formants['F4'], formants['BW1'], formants['BW2'], formants['BW3'], formants['BW4'])
-	formant['time'] = formant['time'] + formant['framelenght']
 
 # Extract F0.
 f0 = dict()
@@ -59,21 +55,17 @@ f0['fields'] = ('Pitch', 'Prob. Voicing', 'Local root mean squared mensurements'
 if f0['method'] == 'esps':
 	f0['time'] = f0['windowlenght'] / 2
 
-#elif f0['method'] == 'amdf':
 else:
 	f0['time'] = 0
 
-print 'Pitchs: '
-for f0_local in f0['processed']:
-
+# Time to print!!!!
+for formant_local, f0_local in izip (formant['processed'], f0['processed']):
 	pitch = dict(zip(f0['fields'], f0_local))
+	formants = dict(zip(formant['fields'], formant_local))
 	if pitch['Prob. Voicing']:
-		print '\tSecond %.2f --> Pitch: %.0f Hz' % (f0['time'], pitch['Pitch'])
+		print '\tSecond %.2f\n\t\tF0: %.0f Hz\n\t\tF1: %.0f Hz\n\t\tF2: %.0f Hz\n\t\tF3: %.0f Hz\n\t\tF4: %.0f Hz\n\t\tBW1: %.0f Hz\n\t\tBW2: %.0f Hz\n\t\tBW3: %.0f Hz\n\t\tBW4: %.0f Hz' % (formant['time'], pitch['Pitch'], formants['F1'], formants['F2'], formants['F3'], formants['F4'], formants['BW1'], formants['BW2'], formants['BW3'], formants['BW4'])
 	f0['time'] = f0['time'] + f0['framelenght']
-
-#	for field, value in pitch.iteritems():
-#		print '\t%s: %s' % (field, value)
-#	print ''
+	formant['time'] = formant['time'] + formant['framelenght']
 
 info = mysound.info()
 info_fields=('Length', 'Rate', 'Maxmimum Sample', 'Minimum Sample', 'Sample encoding', 'Channels', 'Format', 'Header size')
