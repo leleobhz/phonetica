35518f384d665a2509c109c801bff2da8ac27fc3
Leonardo Silva Amaral <contato@leonardoamaral.com.br>
2008-09-16 17:00:17 +0000

    * Improved cepstrum habilities. Now it can run with any sample witdth files and extract F0 mean for each frame separatelly.
    git-svn-id: svn://svn.code.sf.net/p/setfon/code/trunk@43 0b32ef8a-0316-45cc-9536-08b9f7b8220c

--- high_experimental/cepstrum.py
+++ high_experimental/cepstrum.py
@@ -3,6 +3,7 @@
 
 from Numeric import *
 from array import *
+from itertools import izip
 import numpy
 import pylab
 import wave
@@ -13,25 +14,27 @@ import struct
 fp = wave.open(sys.argv[1],"rb")
 
 sample_rate = fp.getframerate()
+sample_width = fp.getsampwidth()
 total_num_samps = fp.getnframes()
-fft_length = int(sys.argv[2])
-num_fft = (total_num_samps / (fft_length*fp._nchannels) ) - 2
+#fft_length = int(sys.argv[2])
+fft_length = 512
+num_fft = (total_num_samps / fft_length ) - 2
 
 # create temporary working array
-temp = zeros((num_fft,fft_length*2),Float)
+temp = zeros((num_fft,fft_length*sample_width),Float)
 
 # read in the data from the file
 for i in range(num_fft):
-    tempb = fp.readframes(fft_length);
-    temp[i,:] = array(Float, struct.unpack("%dB"%(fft_length*2),tempb))
+	tempb = fp.readframes(fft_length);
+	temp[i,:] = array(Float, struct.unpack("%dB"%(fft_length*sample_width),tempb))
 fp.close()
 
-# Window the data
-temp = temp * numpy.hamming(fft_length*2)
+cepstrum = []
 
-# Transform with the FFT, Return Power
-cepstrum  = numpy.fft.fft(log10(1e-20+abs(temp)))
-n_out_pts = (fft_length / 2) + 1
+for i in temp:
+	# Window the data
+	i = i * numpy.hamming(fft_length*sample_width)
+	cepstrum.append(numpy.fft.fft(log10(1e-20+abs(i))))
 
 # Limits
 ms1=sample_rate/1000;                 # maximum speech Fx at 1000Hz
@@ -46,6 +49,9 @@ q = [i/sample_rate for i in range(ms1, ms20)]
 #pylab.ylabel('Amplitude')
 #pylab.show()
 
+for i, j in izip (cepstrum, range(0, num_fft*fft_length, fft_length)):
+	max_cepstrum = numpy.max([abs(x) for x in i[ms1:ms20]])
+	print 'Frame %d Fx=%.0fHz' % (j, sample_rate / (ms1 + int(max_cepstrum) - 1))
 
-max_cepstrum = numpy.max([abs(x) for x in cepstrum[ms1:ms20]])
-print 'Fx=%.0fHz\n' % (sample_rate / (ms1 + int(max_cepstrum) - 1))
+#max_cepstrum = numpy.max([abs(x) for x in cepstrum[ms1:ms20]])
+#print 'Fx=%.0fHz\n' % (sample_rate / (ms1 + int(max_cepstrum) - 1))
