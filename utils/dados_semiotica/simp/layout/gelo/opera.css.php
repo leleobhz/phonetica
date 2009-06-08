be12806d9992840a95028529dcbce8928e1c5d69
Rubens Ribeiro <rubs33>
2009-06-08 14:13:42 +0000

    * Inclusion of layout "gelo" of Web Information System to register for projects, texts, phrases and analyzes.
    git-svn-id: svn://svn.code.sf.net/p/setfon/code/trunk@81 0b32ef8a-0316-45cc-9536-08b9f7b8220c

---
+++ utils/dados_semiotica/simp/layout/gelo/opera.css.php
@@ -0,0 +1,60 @@
+<?php
+//
+// SIMP
+// Descricao: Folha de estilos especificos para navegador Opera
+// Autor: Rubens Takiguti Ribeiro
+// Orgao: TecnoLivre - Cooperativa de Tecnologia e Solucoes Livres
+// E-mail: rubens@tecnolivre.ufla.br
+// Versao: 1.0.0.0
+// Data: 19/02/2008
+// Modificado: 19/02/2008
+// Copyright (C) 2007  Rubens Takiguti Ribeiro
+// License: LICENSE.TXT
+//
+?>
+/*****************
+ * ESTILOS OPERA *
+ *****************/
+
+#navegacao + hr { clear: both; }
+
+<?php
+if ($CFG->transparencia) {
+    echo "#menu {\n";
+    echo "  opacity: {$CFG->transparencia};\n";
+    echo "}\n";
+}
+?>
+
+<?php
+if ($CFG->transparencia) {
+    echo "#menu:hover,\n";
+    echo "#menu:focus,\n";
+    echo "#menu:active {\n";
+    echo "  opacity: {$CFG->opaco};\n";
+    echo "}\n";
+}
+?>
+
+div.erro,
+div.aviso {
+  border-top: 0px;
+  margin-left: -20em;
+  margin-top: 0px;
+  padding-bottom: 5px;
+  position: fixed !important;
+  top: 0px;
+}
+
+div.erro {
+  left: 49%;
+}
+
+div.aviso {
+  left: 51%;
+}
+
+div.erro:hover,
+div.aviso:hover {
+  z-index: 1001;
+}
