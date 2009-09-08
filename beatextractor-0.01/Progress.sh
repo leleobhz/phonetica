5a61ae155a51c3a3f8d25688d8b20696c11b528e
Rubens Ribeiro <rubs33>
2009-09-08 18:02:51 +0000

    Fix bug when file BeatExtractor.kmdr.pid does not exists.
    git-svn-id: svn://svn.code.sf.net/p/setfon/code/trunk@111 0b32ef8a-0316-45cc-9536-08b9f7b8220c

--- beatextractor-0.01/Progress.sh
+++ beatextractor-0.01/Progress.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -x
 
-if [[ -n ~/.bbep/BeatExtractor.kmdr.pid ]]; then $(dcop `cat ~/.bbep/BeatExtractor.kmdr.pid` KommanderIf setText "$1" "$2"); fi
+if [[ -e "$(pwd)/BeatExtractor.kmdr.pid" ]]; then
+    $(dcop `cat ~/.bbep/BeatExtractor.kmdr.pid` KommanderIf setText "$1" "$2")
+fi
